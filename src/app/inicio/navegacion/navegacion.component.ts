b13e9b661eafd80509cc7583e5366e4ae922f141
Jedua <jedua285@gmail.com>
2024-10-14 22:57:56 -0600

    created struct for frontend

--- src/app/inicio/navegacion/navegacion.component.ts
+++ src/app/inicio/navegacion/navegacion.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navegacion',
-  standalone: true,
-  imports: [],
   templateUrl: './navegacion.component.html',
   styleUrl: './navegacion.component.scss'
 })
